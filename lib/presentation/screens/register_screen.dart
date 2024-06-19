c8618682c507b9e03f6a02aa66d0929e3b10194b
hector manuel martinez moreira <pelsmi11@hectors-Mac-mini.local>
2024-06-19 17:33:08 -0600

    feat: form validation Traditional_form

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -37,35 +37,90 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm({super.key});
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  final _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
           CustomTextFormField(
-            label: "Nombre de usuario",
-          ),
+              label: "Nombre de usuario",
+              onChange: (p0) => username = p0,
+              validator: (p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return "El nombre de usuario es requerido";
+                }
+                if (p0.trim().isEmpty) {
+                  return "El nombre de usuario es requerido";
+                }
+                if (p0.trim().length < 6) {
+                  return "mayor a 6 caracteres";
+                }
+
+                return null;
+              }),
           const SizedBox(
             height: 10,
           ),
           CustomTextFormField(
-            label: "Correo electrónico",
-          ),
+              label: "Correo electrónico",
+              onChange: (p0) => email = p0,
+              validator: (p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return "El correo es requerido";
+                }
+                if (p0.trim().isEmpty) {
+                  return "El correo es requerido";
+                }
+                final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                if (!emailRegExp.hasMatch(p0)) {
+                  return "El correo no es válido";
+                }
+
+                return null;
+              }),
           const SizedBox(
             height: 20,
           ),
           CustomTextFormField(
-            label: "Contraseña",
-            obscureText: true,
-          ),
+              label: "Contraseña",
+              obscureText: true,
+              onChange: (p0) => password = p0,
+              validator: (p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return "La contraseña es requerida";
+                }
+                if (p0.trim().isEmpty) {
+                  return "La contraseña es requerida";
+                }
+                if (p0.trim().length < 6) {
+                  return "mayor a 6 caracteres";
+                }
+
+                return null;
+              }),
           const SizedBox(
             height: 20,
           ),
           FilledButton.tonalIcon(
-              onPressed: () {},
+              onPressed: () {
+                final isValid = _formKey.currentState!.validate();
+
+                if (!isValid) return;
+              },
               icon: const Icon(Icons.save),
               label: const Text("Crear usuario")),
           const SizedBox(
